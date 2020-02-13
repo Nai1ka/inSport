91af4433ce5e6a5bd936c405c886d2bb899dc5a0
Nai1ka <nail.minnemullin@mail.ru>
2020-02-14 00:05:54 +0300

    Added new functions

--- app/src/main/java/com/ndevelop/insport/Utils/Utils.java
+++ app/src/main/java/com/ndevelop/insport/Utils/Utils.java
@@ -1,6 +1,14 @@
 package com.ndevelop.insport.Utils;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +19,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 public class Utils {
     public static JSONObject read(Context context, String fileName) {
@@ -33,6 +45,7 @@ public class Utils {
         }
         return null;
     }
+
     public static boolean write(Context context, String fileName, String jsonString) {
         try {
             FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
@@ -46,7 +59,47 @@ public class Utils {
         } catch (IOException ioException) {
             return false;
         }
+    }
 
+    public static Location getLastKnownLocation(Activity activity, Context context) {
+        LocationManager mLocationManager;
+        mLocationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    12);
+        } else {
+            for (String provider : providers) {
+                Location l = mLocationManager.getLastKnownLocation(provider);
+                if (l == null) {
+                    continue;
+                }
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+
+                    bestLocation = l;
+                }
+            }
+        }
+        return bestLocation;
+    }
+
+    public static boolean checkGPSPermissoins(Activity activity) {
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    12);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean gpsStatus(Context context) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+    public static BigDecimal roundUp(float value, int digits) {
+        return new BigDecimal("" + value).setScale(digits, BigDecimal.ROUND_HALF_UP);
     }
 }
